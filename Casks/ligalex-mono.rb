3f1dcd3f6c35b574b9ec09c81bda5147b118742b
DarshanaWT <tharindu24darshana@gmail.com>
2024-02-11 17:42:02 +0800

    Fixed Cask config

--- Casks/ligalex-mono.rb
+++ Casks/ligalex-mono.rb
@@ -2,10 +2,10 @@ cask "ligalex-mono" do
   version :latest
   sha256 :no_check
 
-  url "ligalex-mono", using: :no_check
+  url "ligalex-mono",
   name "ligalex-mono font family"
   desc "A collection of Blex Mono fonts patched with extra ligatures and powerline symbols."
-  homepage "https://github.com/tdarshana/blex-mono-liga-powerline"
+  homepage "https://github.com/tdarshana/homebrew-blex-mono-liga-powerline"
 
   font "ligalex-mono-bold-powerline.ttf"
   font "ligalex-mono-boldItalic-powerline.ttf"
