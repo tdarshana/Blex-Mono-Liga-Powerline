494ebcab623d65eec60595410453cf4f37152c9e
DarshanaWT <tharindu24darshana@gmail.com>
2024-02-11 18:29:25 +0800

    Added sha256 validation for the cask

--- Casks/ligalex-mono.rb
+++ Casks/ligalex-mono.rb
@@ -1,6 +1,6 @@
 cask "ligalex-mono" do
   version :latest
-  sha256 :no_check
+  sha256 "7a68ec36c048402f8874cc0ec7a2cc2762e59aec70505a50b7cebf5c3af4c440"
 
   url "https://github.com/tdarshana/homebrew-blex-mono-liga-powerline/releases/download/rel-v0.1/ligalex-mono-powerline.zip"
   name "Ligalex Mono Font Family"
